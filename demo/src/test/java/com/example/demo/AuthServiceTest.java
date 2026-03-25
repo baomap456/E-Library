8573354b41c3427b5417f3e14ff2872bb8eed4dc
baomap456 <126744704+baomap456@users.noreply.github.com>
2026-03-25 22:24:26 +0700

    update Test

--- demo/src/test/java/com/example/demo/AuthServiceTest.java
+++ demo/src/test/java/com/example/demo/AuthServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.RegisterRequest;
@@ -13,6 +14,7 @@ import com.example.demo.service.AuthService;
 
 @SpringBootTest
 @Transactional // Đảm bảo test xong thì xóa dữ liệu rác đi, không làm bẩn Database thật
+@ActiveProfiles("test")
 public class AuthServiceTest {
 
     @Autowired
