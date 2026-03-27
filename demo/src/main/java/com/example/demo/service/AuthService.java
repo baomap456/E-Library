3928544768f11fe29e8fee7aa4808db904bc9e41
baomap456 <126744704+baomap456@users.noreply.github.com>
2026-03-27 13:25:46 +0700

    DCK-33: #Commnet: Login

--- demo/src/main/java/com/example/demo/service/AuthService.java
+++ demo/src/main/java/com/example/demo/service/AuthService.java
@@ -1,19 +1,22 @@
 package com.example.demo.service;
 
 
-import lombok.RequiredArgsConstructor;
+import java.util.Set;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.model.Role;
 import com.example.demo.model.User;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,32 @@ public class AuthService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder; // Công cụ băm mật khẩu
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        // 1. Nhờ Spring Security kiểm tra username và password
+        // Nếu sai mật khẩu, hàm này sẽ tự động ném ra lỗi (Exception) và dừng lại luôn
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        // 2. Nếu đi qua được bước 1, tức là pass đúng. Ta lấy thông tin User từ DB lên.
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("Không tìm thấy người dùng"));
+
+        // 3. In thẻ JWT mới cho người dùng
+        String jwtToken = jwtService.generateToken(user);
+
+        // 4. Đóng gói Token và trả về cho Frontend
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .username(user.getUsername())
+                .fullName(user.getFullName())
+                .build();
+    }
 
     public AuthResponse register(RegisterRequest request) {
         // 1. Kiểm tra trùng lặp (Validation)
