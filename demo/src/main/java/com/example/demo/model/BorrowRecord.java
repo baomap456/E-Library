d952c4ca9b54284910e0559cbd86ef15f4453667
baomap456 <126744704+baomap456@users.noreply.github.com>
2026-03-27 12:51:43 +0700

    update model

--- demo/src/main/java/com/example/demo/model/BorrowRecord.java
+++ demo/src/main/java/com/example/demo/model/BorrowRecord.java
@@ -2,13 +2,12 @@ package com.example.demo.model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
