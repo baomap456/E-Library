9446d250aa1189dafc9f2e995b20d6df391abe12
baomap456 <126744704+baomap456@users.noreply.github.com>
2026-03-27 12:47:43 +0700

    update BookItem

--- demo/src/main/java/com/example/demo/model/BookItem.java
+++ demo/src/main/java/com/example/demo/model/BookItem.java
@@ -1,6 +1,6 @@
 package com.example.demo.model;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +8,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
